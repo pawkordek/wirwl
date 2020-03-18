240448ec7ff5253ec163bb98afd7aa4e4c9af678
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-03-18 23:11:42 +0100

    Add confirmation dialog

---
+++ internal/widget/confirmation_dialog.go
@@ -0,0 +1,64 @@
+package widget
+
+import "fyne.io/fyne"
+
+/*
+Implementation of a typical yes/no dialog except that confirmation and cancellation are done by using 'y' and 'n' keys
+respectively, therefore no buttons are shown.
+Dialog disappears only when any of those two buttons is pressed.
+Message passed into has an instruction message '(y)es or (n)o' appended at the end so caller doesn't have to add it
+every time.
+*/
+type ConfirmationDialog struct {
+	*MsgPopUp
+	OnConfirm func()
+	OnCancel  func()
+	focused   bool
+}
+
+func NewConfirmationDialog(canvas fyne.Canvas) *ConfirmationDialog {
+	dialog := ConfirmationDialog{}
+	dialog.MsgPopUp = NewMsgPopUp(canvas)
+	dialog.OnConfirm = func() {}
+	dialog.OnCancel = func() {}
+	dialog.ExtendBaseWidget(dialog)
+	return &dialog
+}
+
+func (dialog *ConfirmationDialog) TypedKey(key *fyne.KeyEvent) {
+	if key.Name == fyne.KeyY {
+		dialog.OnConfirm()
+		dialog.Hide()
+		dialog.Canvas.Unfocus()
+	} else if key.Name == fyne.KeyN {
+		dialog.OnCancel()
+		dialog.Hide()
+		dialog.Canvas.Unfocus()
+	}
+}
+
+func (dialog *ConfirmationDialog) SetMsg(msg string) {
+	dialog.MsgPopUp.SetMsg(msg)
+	dialog.msg.Text += " (y)es or (n)o?"
+}
+
+func (dialog *ConfirmationDialog) Display(msg string) {
+	dialog.MsgPopUp.Display(InfoPopUp, msg)
+	dialog.Canvas.Focus(dialog)
+}
+
+func (dialog *ConfirmationDialog) FocusGained() {
+	dialog.focused = true
+}
+
+func (dialog *ConfirmationDialog) FocusLost() {
+	dialog.focused = false
+}
+
+func (dialog *ConfirmationDialog) Focused() bool {
+	return dialog.focused
+}
+
+func (dialog *ConfirmationDialog) TypedRune(r rune) {
+	//Do nothing as inputting text handling is not needed, only key presses
+}
---
+++ internal/widget/confirmation_dialog_test.go
@@ -0,0 +1,36 @@
+package widget
+
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/test"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestThatConfirmingWorksCorrectly(t *testing.T) {
+	called := false
+	dialog := NewConfirmationDialog(test.Canvas())
+	dialog.OnConfirm = func() { called = true }
+	dialog.Display("")
+	dialog.SimulateKeyPress(fyne.KeyY)
+	assert.Equal(t, true, called)
+	assert.Equal(t, true, dialog.Hidden)
+	assert.Equal(t, false, dialog.Focused())
+}
+
+func TestThatCancellingWorksCorrectly(t *testing.T) {
+	called := false
+	dialog := NewConfirmationDialog(test.Canvas())
+	dialog.OnCancel = func() { called = true }
+	dialog.Display("")
+	dialog.SimulateKeyPress(fyne.KeyN)
+	assert.Equal(t, true, called)
+	assert.Equal(t, true, dialog.Hidden)
+	assert.Equal(t, false, dialog.Focused())
+}
+
+func TestThatDisplayedMessageHasYesOrNoMessageAppended(t *testing.T) {
+	dialog := NewConfirmationDialog(test.Canvas())
+	dialog.SetMsg("Some message.")
+	assert.Equal(t, "Some message. (y)es or (n)o?", dialog.Msg())
+}
