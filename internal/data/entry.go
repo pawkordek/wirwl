1d29f5d670693da80f967e6f2e438daf68e883f7
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-03-22 12:16:12 +0100

    Move Entry struct to a separate file
    There will be more entry related code in the future so separating
    provider from entry will allow to find it easier.

---
+++ internal/data/entry.go
@@ -0,0 +1,14 @@
+package data
+
+type Entry struct {
+	Id                         int
+	Status                     string
+	Title                      string
+	Completion                 int
+	AmountOfElementsToComplete int
+	Score                      int
+	Link                       string
+	Description                string
+	Comment                    string
+	MediaType                  string
+}
