067d89cbf03c3d5149dd8805f7a9b3aa529b56e3
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-04-19 08:38:01 +0200

    Simplify App tests by making functions out of most used operations
    This saves space, makes tests more easily readible and makes it easy to
    update code with any changes to inputs for commonly used operations.

--- internal/test_helpers_test.go
+++ internal/test_helpers_test.go
@@ -12,3 +12,45 @@ func (app *App) SimulateKeyPress(key fyne.KeyName) {
 		onTypedKey(event)
 	}
 }
+
+func (app *App) SimulateSwitchingToNextEntryType() {
+	app.SimulateKeyPress(fyne.KeyL)
+}
+
+func (app *App) SimulateSwitchingToPreviousEntryType() {
+	app.SimulateKeyPress(fyne.KeyH)
+}
+
+func (app *App) SimulateOpeningDialogForAddingEntryType() {
+	app.SimulateKeyPress(fyne.KeyT)
+	app.SimulateKeyPress(fyne.KeyI)
+}
+
+func (app *App) SimulateAddingNewEntryTypeWithName(text string) {
+	app.SimulateOpeningDialogForAddingEntryType()
+	app.SimulateKeyPress(fyne.KeyI)
+	app.addEntryTypeDialog.Type(text)
+	app.SimulateKeyPress(fyne.KeyEnter)
+}
+
+func (app *App) SimulateSavingChanges() {
+	app.SimulateKeyPress(fyne.KeyS)
+}
+
+func (app *App) SimulateAttemptAtDeletionOfCurrentEntryType() {
+	app.SimulateKeyPress(fyne.KeyT)
+	app.SimulateKeyPress(fyne.KeyD)
+}
+
+func (app *App) SimulateDeletionOfCurrentEntryType() {
+	app.SimulateAttemptAtDeletionOfCurrentEntryType()
+	app.SimulateKeyPress(fyne.KeyY)
+}
+
+func (app *App) SimulateEditionOfCurrentEntryTypeTo(text string) {
+	app.SimulateKeyPress(fyne.KeyT)
+	app.SimulateKeyPress(fyne.KeyE)
+	app.SimulateKeyPress(fyne.KeyI)
+	app.editEntryTypeDialog.Type(text)
+	app.SimulateKeyPress(fyne.KeyEnter)
+}
