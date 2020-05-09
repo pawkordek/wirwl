00870372006bcefe538fa5bbd79d40d25e7b1bc3
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-05-09 14:01:26 +0200

    Add command line arguments for app directories to the app

--- cmd/wirwl/main.go
+++ cmd/wirwl/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fyne.io/fyne/app"
 	wirwl "wirwl/internal"
 )
 
 func main() {
+	flags := readCommandLineFlags()
 	wirwlApp := wirwl.NewApp(app.New())
-	wirwlApp.LoadAndDisplay("./", "./")
+	wirwlApp.LoadAndDisplay(flags["configDirPath"], flags["appDataDirPath"])
+}
+
+func readCommandLineFlags() map[string]string {
+	configDirPath := flag.String("c", "", "A path to a directory containing the application config file. If not provided it will default to [HOME]/.local/share/wirwl/")
+	appDataDirPath := flag.String("ad", "", "A path to a directory containing the application data. If not provided it will default to [HOME]/.local/share/wirwl/")
+	flag.Parse()
+	return map[string]string{
+		"configDirPath":  *configDirPath,
+		"appDataDirPath": *appDataDirPath,
+	}
 }
