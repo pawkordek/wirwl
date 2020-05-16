4b6a496267b0af9f612d54d9d1e5b9f43b67b576
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-05-16 23:06:35 +0200

    Add function for deleting all files/directories in a directory

--- internal/data/io.go
+++ internal/data/io.go
@@ -2,14 +2,29 @@ package data
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func DeleteFile(path string) {
 	_, err := os.Stat(path)
 	if !os.IsNotExist(err) {
 		err = os.RemoveAll(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func DeleteAllInDir(dirPath string) {
+	subDirs, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, subDir := range subDirs {
+		err = os.RemoveAll(filepath.Join(dirPath, subDir.Name()))
 		if err != nil {
 			log.Fatal(err)
 		}
