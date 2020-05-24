134371d353b793a4ba10cb7bceef39a26d67cdd9
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-05-24 17:04:45 +0200

    Remove redundant appDataDirPath (ad) command line parameter
    It isn't used anymore as appDataDirPath is taken directly from config
    file now.

--- cmd/wirwl/main.go
+++ cmd/wirwl/main.go
@@ -15,10 +15,8 @@ func main() {
 
 func readCommandLineFlags() map[string]string {
 	configDirPath := flag.String("c", "", "A path to a directory containing the application config file. If not provided it will default to [HOME]/.config/wirwl/")
-	appDataDirPath := flag.String("ad", "", "A path to a directory containing the application data. If not provided it will default to [HOME]/.local/share/wirwl/")
 	flag.Parse()
 	return map[string]string{
-		"configDirPath":  *configDirPath,
-		"appDataDirPath": *appDataDirPath,
+		"configDirPath": *configDirPath,
 	}
 }
