89c63038cbc5e3fd8fadb6bfe3ed119ef5c448db
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-07-26 10:22:51 +0200

    Move app input simulation functions out of common test helpers
    They don't really belong in here as they're input related and this is not
    commonly used in tests.

---
+++ internal/test_app_input_helpers_test.go
@@ -0,0 +1,57 @@
+package wirwl
+
+import "fyne.io/fyne"
+
+func (app *App) simulateKeyPress(key fyne.KeyName) {
+	event := &fyne.KeyEvent{Name: key}
+	focusedElement := app.mainWindow.Canvas().Focused()
+	if focusedElement != nil {
+		focusedElement.TypedKey(event)
+	} else {
+		onTypedKey := app.mainWindow.Canvas().OnTypedKey()
+		onTypedKey(event)
+	}
+}
+
+func (app *App) simulateSwitchingToNextEntryType() {
+	app.simulateKeyPress(fyne.KeyL)
+}
+
+func (app *App) simulateSwitchingToPreviousEntryType() {
+	app.simulateKeyPress(fyne.KeyH)
+}
+
+func (app *App) simulateOpeningDialogForAddingEntryType() {
+	app.simulateKeyPress(fyne.KeyT)
+	app.simulateKeyPress(fyne.KeyI)
+}
+
+func (app *App) simulateAddingNewEntryTypeWithName(text string) {
+	app.simulateOpeningDialogForAddingEntryType()
+	app.simulateKeyPress(fyne.KeyI)
+	app.addEntryTypeDialog.Type(text)
+	app.simulateKeyPress(fyne.KeyEnter)
+}
+
+func (app *App) simulateSavingChanges() {
+	app.simulateKeyPress(fyne.KeyS)
+	app.simulateKeyPress(fyne.KeyY)
+}
+
+func (app *App) simulateAttemptAtDeletionOfCurrentEntryType() {
+	app.simulateKeyPress(fyne.KeyT)
+	app.simulateKeyPress(fyne.KeyD)
+}
+
+func (app *App) simulateDeletionOfCurrentEntryType() {
+	app.simulateAttemptAtDeletionOfCurrentEntryType()
+	app.simulateKeyPress(fyne.KeyY)
+}
+
+func (app *App) simulateEditionOfCurrentEntryTypeTo(text string) {
+	app.simulateKeyPress(fyne.KeyT)
+	app.simulateKeyPress(fyne.KeyE)
+	app.simulateKeyPress(fyne.KeyI)
+	app.editEntryTypeDialog.Type(text)
+	app.simulateKeyPress(fyne.KeyEnter)
+}
--- internal/test_helpers_test.go
+++ internal/test_helpers_test.go
@@ -2,7 +2,6 @@ package wirwl
 
 import (
 	"bytes"
-	"fyne.io/fyne"
 	"github.com/BurntSushi/toml"
 	"io"
 	"os"
@@ -202,58 +201,4 @@ func (config *Config) saveConfigIn(configFilePath string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func (app *App) simulateKeyPress(key fyne.KeyName) {
-	event := &fyne.KeyEvent{Name: key}
-	focusedElement := app.mainWindow.Canvas().Focused()
-	if focusedElement != nil {
-		focusedElement.TypedKey(event)
-	} else {
-		onTypedKey := app.mainWindow.Canvas().OnTypedKey()
-		onTypedKey(event)
-	}
-}
-
-func (app *App) simulateSwitchingToNextEntryType() {
-	app.simulateKeyPress(fyne.KeyL)
-}
-
-func (app *App) simulateSwitchingToPreviousEntryType() {
-	app.simulateKeyPress(fyne.KeyH)
-}
-
-func (app *App) simulateOpeningDialogForAddingEntryType() {
-	app.simulateKeyPress(fyne.KeyT)
-	app.simulateKeyPress(fyne.KeyI)
-}
-
-func (app *App) simulateAddingNewEntryTypeWithName(text string) {
-	app.simulateOpeningDialogForAddingEntryType()
-	app.simulateKeyPress(fyne.KeyI)
-	app.addEntryTypeDialog.Type(text)
-	app.simulateKeyPress(fyne.KeyEnter)
-}
-
-func (app *App) simulateSavingChanges() {
-	app.simulateKeyPress(fyne.KeyS)
-	app.simulateKeyPress(fyne.KeyY)
-}
-
-func (app *App) simulateAttemptAtDeletionOfCurrentEntryType() {
-	app.simulateKeyPress(fyne.KeyT)
-	app.simulateKeyPress(fyne.KeyD)
-}
-
-func (app *App) simulateDeletionOfCurrentEntryType() {
-	app.simulateAttemptAtDeletionOfCurrentEntryType()
-	app.simulateKeyPress(fyne.KeyY)
-}
-
-func (app *App) simulateEditionOfCurrentEntryTypeTo(text string) {
-	app.simulateKeyPress(fyne.KeyT)
-	app.simulateKeyPress(fyne.KeyE)
-	app.simulateKeyPress(fyne.KeyI)
-	app.editEntryTypeDialog.Type(text)
-	app.simulateKeyPress(fyne.KeyEnter)
 }
