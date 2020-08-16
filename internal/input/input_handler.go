357eeb029431f159cd9509733dab260994af1ac7
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-08-16 22:42:16 +0200

    Add ability for InputHandler to handle the same action for many callers

--- internal/input/input_handler.go
+++ internal/input/input_handler.go
@@ -13,16 +13,25 @@ type KeyCombination struct {
 	secondKey fyne.KeyName
 }
 
+//Caller should be anything that allows to unambiguously find the correct action for that caller
+//That is, if there are various objects that use the same struct (e.g. many copies of certain widget), it's best
+//to pass the object itself as this guarantees unambiguity
+//If struct only ever has one instance, it might be fine to pass something else, e.g. a string (like in App's case)
+type CallerActionPair struct {
+	caller interface{}
+	action Action
+}
+
 //Stores key combinations mapped to actions. Every action that should be handled, should have a function bound to it
 //which will get executed when key combination for that action gets pressed
 type InputHandler struct {
 	keymap  map[KeyCombination]Action
-	actions map[Action]func()
+	actions map[CallerActionPair]func()
 	lastKey fyne.KeyName
 }
 
 func NewInputHandler(keymap map[string]Action) InputHandler {
-	handler := InputHandler{keymap: map[KeyCombination]Action{}, actions: map[Action]func(){}}
+	handler := InputHandler{keymap: map[KeyCombination]Action{}, actions: map[CallerActionPair]func(){}}
 	handler.createActualKeymap(keymap)
 	return handler
 }
@@ -48,15 +57,23 @@ func getKeyCombinationFromStringKey(key string) KeyCombination {
 	}
 }
 
-func (handler *InputHandler) BindFunctionToAction(action Action, function func()) {
-	handler.actions[action] = function
+func (handler *InputHandler) BindFunctionToAction(caller interface{}, action Action, function func()) {
+	callerActionPair := CallerActionPair{
+		caller: caller,
+		action: action,
+	}
+	handler.actions[callerActionPair] = function
 }
 
-func (handler *InputHandler) Handle(keyName fyne.KeyName) {
+func (handler *InputHandler) Handle(caller interface{}, keyName fyne.KeyName) {
 	for keyCombination, action := range handler.keymap {
 		if (keyCombination.secondKey == keyName && keyCombination.firstKey == handler.lastKey) ||
 			(keyCombination.firstKey == keyName && keyCombination.secondKey == "") {
-			handler.actions[action]()
+			callerActionPair := CallerActionPair{
+				caller: caller,
+				action: action,
+			}
+			handler.actions[callerActionPair]()
 			break
 		}
 	}
--- internal/input/input_handler_test.go
+++ internal/input/input_handler_test.go
@@ -14,8 +14,8 @@ func TestThatInputHandlerHandlesSingleKeyActions(t *testing.T) {
 	keymap := make(map[string]Action)
 	keymap["Q"] = testAction
 	inputHandler := NewInputHandler(keymap)
-	inputHandler.BindFunctionToAction(testAction, function)
-	inputHandler.Handle(fyne.KeyQ)
+	inputHandler.BindFunctionToAction("", testAction, function)
+	inputHandler.Handle("", fyne.KeyQ)
 	assert.True(t, functionExecuted)
 }
 
@@ -25,9 +25,26 @@ func TestThatInputHandlerHandlesActionsInKeySequences(t *testing.T) {
 	keymap := make(map[string]Action)
 	keymap["Z,X"] = testAction
 	inputHandler := NewInputHandler(keymap)
-	inputHandler.BindFunctionToAction(testAction, function)
-	inputHandler.Handle(fyne.KeyZ)
+	inputHandler.BindFunctionToAction("", testAction, function)
+	inputHandler.Handle("", fyne.KeyZ)
 	assert.False(t, functionExecuted)
-	inputHandler.Handle(fyne.KeyX)
+	inputHandler.Handle("", fyne.KeyX)
 	assert.True(t, functionExecuted)
+}
+
+func TestThatInputHandlerHandlesActionForTheCorrectCaller(t *testing.T) {
+	firstCallerFunctionExecuted := false
+	secondCallerFunctionExecuted := false
+	firstCallerFunction := func() { firstCallerFunctionExecuted = true }
+	secondCallerFunction := func() { secondCallerFunctionExecuted = true }
+	keymap := make(map[string]Action)
+	keymap["T"] = testAction
+	inputHandler := NewInputHandler(keymap)
+	inputHandler.BindFunctionToAction("firstCaller", testAction, firstCallerFunction)
+	inputHandler.BindFunctionToAction("secondCaller", testAction, secondCallerFunction)
+	inputHandler.Handle("firstCaller", fyne.KeyT)
+	assert.True(t, firstCallerFunctionExecuted)
+	assert.False(t, secondCallerFunctionExecuted)
+	inputHandler.Handle("secondCaller", fyne.KeyT)
+	assert.True(t, secondCallerFunctionExecuted)
 }
