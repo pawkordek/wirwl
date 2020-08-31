10f6e6ef8d788d6d2b6c13f9e2b8b26e0a2875ed
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-08-31 18:34:52 +0200

    Make Entry's naming of elements completion related variables more clear

--- internal/data/entry.go
+++ internal/data/entry.go
@@ -3,18 +3,18 @@ package data
 import "fmt"
 
 type Entry struct {
-	Id                         int
-	Status                     string
-	Title                      string
-	Completion                 int
-	AmountOfElementsToComplete int
-	Score                      int
-	Link                       string
-	Description                string
-	Comment                    string
-	MediaType                  string
-	Tags                       string
-	ImageQuery                 string
+	Id                              int
+	Status                          string
+	Title                           string
+	ElementsCompleted               int
+	TotalAmountOfElementsToComplete int
+	Score                           int
+	Link                            string
+	Description                     string
+	Comment                         string
+	MediaType                       string
+	Tags                            string
+	ImageQuery                      string
 }
 
 func (entry Entry) String() string {
