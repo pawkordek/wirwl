56bf1e0dc939318be3575097f3b0c7b80fe8cd99
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-09-07 23:47:27 +0200

    Fix numeric input field not rendering properly

--- internal/widget/numeric_input_field.go
+++ internal/widget/numeric_input_field.go
@@ -11,9 +11,16 @@ type NumericInputField struct {
 }
 
 func NewNumericInputField(canvas fyne.Canvas, inputHandler input.Handler) *NumericInputField {
-	inputField := &NumericInputField{
-		InputField: *NewInputField(canvas, inputHandler),
-	}
+	//The order of things is a workaround for a possible bug in fyne where numeric input field doesn't refresh (draw correctly)
+	//unless it's base widget is the one that is set (it can't be done by extend as no matter what the order of operations
+	//is, input field as a parent will always extend it overriding whatever numeric input field sets)
+	//Seems like a bug as other widgets that are extended multiple times don't have this problem
+	//TODO: Check
+	inputField := &NumericInputField{}
+	inputField.ExtendBaseWidget(inputField)
+	baseWidget := inputField.BaseWidget
+	inputField.InputField = *NewInputField(canvas, inputHandler)
+	inputField.BaseWidget = baseWidget
 	inputField.SetRuneFilteringFunction(func(r rune) bool {
 		return unicode.IsDigit(r)
 	})
