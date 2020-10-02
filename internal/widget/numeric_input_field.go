85858faafe89ec2a527765dbc327a86d7c832875
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-10-02 22:03:37 +0200

    Make NumericInputField embed InputField properly without weird workaround

--- internal/widget/numeric_input_field.go
+++ internal/widget/numeric_input_field.go
@@ -7,20 +7,14 @@ import (
 )
 
 type NumericInputField struct {
-	InputField
+	*InputField
 }
 
 func NewNumericInputField(canvas fyne.Canvas, inputHandler input.Handler) *NumericInputField {
-	//The order of things is a workaround for a possible bug in fyne where numeric input field doesn't refresh (draw correctly)
-	//unless it's base widget is the one that is set (it can't be done by extend as no matter what the order of operations
-	//is, input field as a parent will always extend it overriding whatever numeric input field sets)
-	//Seems like a bug as other widgets that are extended multiple times don't have this problem
-	//TODO: Check
-	inputField := &NumericInputField{}
+	inputField := &NumericInputField{
+		InputField: newInputField(canvas, inputHandler),
+	}
 	inputField.ExtendBaseWidget(inputField)
-	baseWidget := inputField.BaseWidget
-	inputField.InputField = *NewInputField(canvas, inputHandler)
-	inputField.BaseWidget = baseWidget
 	inputField.SetRuneFilteringFunction(func(r rune) bool {
 		return unicode.IsDigit(r)
 	})
