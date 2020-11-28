d9f7ec38d2424d2aa5542e80fd4dacaf4607070c
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2020-11-28 21:13:59 +0100

    Extract add entry type dialog functionality into separate file

---
+++ internal/add_entry_type_dialog.go
@@ -0,0 +1,47 @@
+package wirwl
+
+import (
+	"wirwl/internal/data"
+	"wirwl/internal/widget"
+)
+
+func (app *App) createAddEntryTypeDialog() {
+	app.addEntryTypeDialog = widget.NewFormDialog(app.mainWindow.Canvas(), app.inputHandler, "Add new entry type", app.createEntryTypeRelatedDialogElements()...)
+	app.addEntryTypeDialog.OnEnterPressed = app.onEnterPressedInAddEntryTypeDialog
+}
+
+func (app *App) onEnterPressedInAddEntryTypeDialog() {
+	currentTabText := app.getCurrentTabText()
+	err := app.addNewEntryType()
+	if err != nil {
+		app.msgDialog.SetOneTimeOnHideCallback(func() {
+			app.addEntryTypeDialog.Display()
+		})
+		app.msgDialog.Display(widget.ErrorPopUp, err.Error())
+	} else {
+		for _, tab := range app.entriesTypesTabs.Items() {
+			if tab.Text == currentTabText {
+				app.entriesTypesTabs.SelectTab(tab)
+				break
+			}
+		}
+	}
+}
+
+func (app *App) addNewEntryType() error {
+	newEntryType := app.getNewEntryType()
+	err := app.entriesContainer.AddEntryType(newEntryType)
+	if err != nil {
+		return err
+	}
+	app.loadEntriesTypesTabs()
+	app.prepareMainWindowContent()
+	return nil
+}
+
+func (app *App) getNewEntryType() data.EntryType {
+	return data.EntryType{
+		Name:       app.addEntryTypeDialog.ItemValue("Name"),
+		ImageQuery: app.addEntryTypeDialog.ItemValue("Image query"),
+	}
+}
--- internal/app.go
+++ internal/app.go
@@ -142,8 +142,7 @@ func (app *App) prepareDialogs() {
 	app.msgDialog = widget.NewMsgPopUp(app.mainWindow.Canvas())
 	app.confirmationDialog = widget.NewConfirmationDialog(app.mainWindow.Canvas())
 	app.confirmationDialog.OnConfirm = app.deleteCurrentEntryType
-	app.addEntryTypeDialog = widget.NewFormDialog(app.mainWindow.Canvas(), app.inputHandler, "Add new entry type", app.createEntryTypeRelatedDialogElements()...)
-	app.addEntryTypeDialog.OnEnterPressed = app.onEnterPressedInAddEntryTypeDialog
+	app.createAddEntryTypeDialog()
 	app.editEntryTypeDialog = widget.NewFormDialog(app.mainWindow.Canvas(), app.inputHandler, "Editing entry type: "+app.getCurrentTabText(), app.createEntryTypeRelatedDialogElements()...)
 	app.editEntryTypeDialog.OnEnterPressed = app.applyChangesToCurrentEntryType
 }
@@ -175,24 +174,6 @@ func (app *App) getCurrentEntryType() data.EntryType {
 		log.Error(err)
 	}
 	return currentEntryType
-}
-
-func (app *App) onEnterPressedInAddEntryTypeDialog() {
-	currentTabText := app.getCurrentTabText()
-	err := app.addNewEntryType()
-	if err != nil {
-		app.msgDialog.SetOneTimeOnHideCallback(func() {
-			app.addEntryTypeDialog.Display()
-		})
-		app.msgDialog.Display(widget.ErrorPopUp, err.Error())
-	} else {
-		for _, tab := range app.entriesTypesTabs.Items() {
-			if tab.Text == currentTabText {
-				app.entriesTypesTabs.SelectTab(tab)
-				break
-			}
-		}
-	}
 }
 
 func (app *App) applyChangesToCurrentEntryType() {
@@ -250,24 +231,6 @@ func (app *App) trySavingChangesToDb() {
 		app.msgDialog.Display(widget.ErrorPopUp, err.Error())
 	} else {
 		app.msgDialog.Display(widget.SuccessPopUp, "Changes saved.")
-	}
-}
-
-func (app *App) addNewEntryType() error {
-	newEntryType := app.getNewEntryType()
-	err := app.entriesContainer.AddEntryType(newEntryType)
-	if err != nil {
-		return err
-	}
-	app.loadEntriesTypesTabs()
-	app.prepareMainWindowContent()
-	return nil
-}
-
-func (app *App) getNewEntryType() data.EntryType {
-	return data.EntryType{
-		Name:       app.addEntryTypeDialog.ItemValue("Name"),
-		ImageQuery: app.addEntryTypeDialog.ItemValue("Image query"),
 	}
 }
 
