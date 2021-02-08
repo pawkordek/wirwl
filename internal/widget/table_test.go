70c2beea215b0efaef380e91022e3b96ddf9f21b
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2021-02-08 21:31:49 +0100

    Remove code duplication in table tests

--- internal/widget/table_test.go
+++ internal/widget/table_test.go
@@ -16,6 +16,9 @@ const expectedColumnWidth = 100
 const expectedColumnWidthWithPadding = 135
 const expectedRowHeight = 141
 
+const testColumnAmount = 14
+const testRowAmount = 20
+
 func createLabelsForTesting(amountOfLabels int) []fyne.CanvasObject {
 	labels := []fyne.CanvasObject{}
 	for i := 1; i <= amountOfLabels; i++ {
@@ -26,18 +29,27 @@ func createLabelsForTesting(amountOfLabels int) []fyne.CanvasObject {
 	return labels
 }
 
+func createTableRendererForTesting(tableColumnAmount int, tableRowAmount int) tableRenderer {
+	table := NewTable(testColumnAmount, createLabelsForTesting(testColumnAmount), createLabelsForTesting(testColumnAmount*testRowAmount))
+	renderer := table.CreateRenderer().(tableRenderer)
+	//The size is arbitrary but shouldn't be zero as layout with zero size doesn't make any sense
+	renderer.Layout(fyne.NewSize(1000, 1000))
+	return renderer
+}
+
+func createTableForTesting(columnAmount int, rowAmount int) Table {
+	renderer := createTableRendererForTesting(columnAmount, rowAmount)
+	return renderer.table
+}
+
 func TestThatTableHasCorrectMinSize(t *testing.T) {
-	const columnAmount = 14
-	const rowAmount = 100
-	table := NewTable(columnAmount, []fyne.CanvasObject{}, createLabelsForTesting(columnAmount*rowAmount))
-	minSize := table.MinSize()
-	assert.Equal(t, columnAmount*testLabelWidth, minSize.Width, "Table has incorrect minimum width")
-	assert.Equal(t, rowAmount*testLabelHeight, minSize.Height, "Table has incorrect minimum height")
+	table := createTableForTesting(testColumnAmount, testRowAmount)
+	assert.Equal(t, testColumnAmount*testLabelWidth, table.MinSize().Width, "Table has incorrect minimum width")
+	assert.Equal(t, testRowAmount*testLabelHeight, table.MinSize().Height, "Table has incorrect minimum height")
 }
 
 func TestThatObjectsInHeaderHaveCorrectPositions(t *testing.T) {
-	table := NewTable(14, createLabelsForTesting(14), []fyne.CanvasObject{})
-	table.CreateRenderer().Layout(fyne.NewSize(1000, 1000))
+	table := createTableForTesting(testColumnAmount, testRowAmount)
 	posX := 0
 	posY := 0
 	for i, object := range table.headerObjects {
@@ -48,8 +60,7 @@ func TestThatObjectsInHeaderHaveCorrectPositions(t *testing.T) {
 }
 
 func TestThatObjectsInHeaderHaveCorrectSize(t *testing.T) {
-	table := NewTable(14, createLabelsForTesting(14), []fyne.CanvasObject{})
-	table.CreateRenderer().Layout(fyne.NewSize(1000, 1000))
+	table := createTableForTesting(testColumnAmount, testRowAmount)
 	for i, object := range table.headerObjects {
 		assert.Equal(t, expectedColumnWidth, object.Size().Width, "Width of object num "+strconv.Itoa(i)+" is incorrect")
 		assert.Equal(t, expectedHeaderHeight, object.Size().Height, "Height of object num "+strconv.Itoa(i)+" is incorrect")
@@ -57,17 +68,14 @@ func TestThatObjectsInHeaderHaveCorrectSize(t *testing.T) {
 }
 
 func TestThatObjectsThatCreateDataRowsHaveCorrectPositions(t *testing.T) {
-	const columnAmount = 14
-	const rowAmount = 10
-	table := NewTable(columnAmount, []fyne.CanvasObject{}, createLabelsForTesting(columnAmount*rowAmount))
-	table.CreateRenderer().Layout(fyne.NewSize(1000, 1000))
+	table := createTableForTesting(testColumnAmount, testRowAmount)
 	posX := 0
 	posY := expectedHeaderHeight
 	for i, object := range table.objects {
 		assert.Equal(t, posX, object.Position().X, "Position x of object num "+strconv.Itoa(i)+" is incorrect")
 		assert.Equal(t, posY, object.Position().Y, "Position y of object num "+strconv.Itoa(i)+" is incorrect")
 		posX += expectedColumnWidthWithPadding
-		if i != 0 && (i+1)%columnAmount == 0 {
+		if i != 0 && (i+1)%testColumnAmount == 0 {
 			posX = 0
 			posY += expectedRowHeight
 		}
@@ -75,10 +83,7 @@ func TestThatObjectsThatCreateDataRowsHaveCorrectPositions(t *testing.T) {
 }
 
 func TestThatObjectsThatCreateDataRowsHaveCorrectSize(t *testing.T) {
-	const columnAmount = 14
-	const rowAmount = 10
-	table := NewTable(columnAmount, []fyne.CanvasObject{}, createLabelsForTesting(columnAmount*rowAmount))
-	table.CreateRenderer().Layout(fyne.NewSize(1000, 1000))
+	table := createTableForTesting(testColumnAmount, testRowAmount)
 	for i, object := range table.objects {
 		assert.Equal(t, expectedColumnWidth, object.Size().Width, "Width of object num "+strconv.Itoa(i)+" is incorrect")
 		assert.Equal(t, expectedRowHeight, object.Size().Height, "Height of object num "+strconv.Itoa(i)+" is incorrect")
@@ -86,12 +91,9 @@ func TestThatObjectsThatCreateDataRowsHaveCorrectSize(t *testing.T) {
 }
 
 func TestThatHeaderRowBorderIsDrawnCorrectly(t *testing.T) {
-	const columnAmount = 14
-	table := NewTable(columnAmount, createLabelsForTesting(14), []fyne.CanvasObject{})
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
 	rectangle := renderer.headerRowBorder
-	assert.Equal(t, columnAmount*expectedColumnWidthWithPadding, rectangle.Size().Width)
+	assert.Equal(t, testColumnAmount*expectedColumnWidthWithPadding, rectangle.Size().Width)
 	assert.Equal(t, expectedHeaderHeight, rectangle.Size().Height)
 	assert.Equal(t, float32(2), rectangle.StrokeWidth)
 	assert.Equal(t, color.Black, rectangle.StrokeColor)
@@ -99,32 +101,20 @@ func TestThatHeaderRowBorderIsDrawnCorrectly(t *testing.T) {
 }
 
 func TestThatThereIsCorrectAmountOfDataRowBorders(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
-	assert.Equal(t, amountOfRows, len(renderer.dataRowsBorders))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
+	assert.Equal(t, testRowAmount, len(renderer.dataRowsBorders))
 }
 
 func TestThatAllDataRowBordersHaveCorrectSize(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
 	for i, border := range renderer.dataRowsBorders {
-		assert.Equal(t, columnAmount*expectedColumnWidthWithPadding, border.Size().Width, "Border with number "+strconv.Itoa(i)+" does not have the correct width")
+		assert.Equal(t, testColumnAmount*expectedColumnWidthWithPadding, border.Size().Width, "Border with number "+strconv.Itoa(i)+" does not have the correct width")
 		assert.Equal(t, expectedRowHeight, border.Size().Height, "Border with number "+strconv.Itoa(i)+" does not have the correct height")
 	}
 }
 
 func TestThatAllDataBordersHaveCorrectPosition(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
 	expectedPosition := fyne.NewPos(0, expectedHeaderHeight)
 	for i, border := range renderer.dataRowsBorders {
 		assert.Equal(t, expectedPosition, border.Position(), "Border with number "+strconv.Itoa(i)+" does not have correct position")
@@ -133,11 +123,7 @@ func TestThatAllDataBordersHaveCorrectPosition(t *testing.T) {
 }
 
 func TestThatAllDataBordersHaveCorrectProperties(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
 	for i, border := range renderer.dataRowsBorders {
 		assert.Equal(t, float32(2), border.StrokeWidth, "Border with number "+strconv.Itoa(i)+" does not have correct stroke width")
 		assert.Equal(t, color.Black, border.StrokeColor, "Border with number "+strconv.Itoa(i)+" does not have correct stroke color")
@@ -146,32 +132,20 @@ func TestThatAllDataBordersHaveCorrectProperties(t *testing.T) {
 }
 
 func TestThatThereIsCorrectAmountOfColumnBorders(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
-	assert.Equal(t, columnAmount, len(renderer.columnBorders))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
+	assert.Equal(t, testColumnAmount, len(renderer.columnBorders))
 }
 
 func TestThatAllColumnBordersHaveCorrectSize(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
 	for i, border := range renderer.columnBorders {
 		assert.Equal(t, expectedColumnWidthWithPadding, border.Size().Width, "Border with number "+strconv.Itoa(i)+" does not have the correct width")
-		assert.Equal(t, expectedHeaderHeight+expectedRowHeight*amountOfRows, border.Size().Height, "Border with number "+strconv.Itoa(i)+" does not have the correct height")
+		assert.Equal(t, expectedHeaderHeight+expectedRowHeight*testRowAmount, border.Size().Height, "Border with number "+strconv.Itoa(i)+" does not have the correct height")
 	}
 }
 
 func TestThatAllColumnBordersHaveCorrectPosition(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
 	expectedPosition := fyne.NewPos(0, 0)
 	for i, border := range renderer.columnBorders {
 		assert.Equal(t, expectedPosition, border.Position(), "Border with number "+strconv.Itoa(i)+" does not have correct position")
@@ -180,11 +154,7 @@ func TestThatAllColumnBordersHaveCorrectPosition(t *testing.T) {
 }
 
 func TestThatAllColumnBordersHaveCorrectProperties(t *testing.T) {
-	const columnAmount = 14
-	const amountOfRows = 20
-	table := NewTable(columnAmount, createLabelsForTesting(columnAmount), createLabelsForTesting(columnAmount*amountOfRows))
-	renderer := table.CreateRenderer().(tableRenderer)
-	renderer.Layout(fyne.NewSize(1000, 1000))
+	renderer := createTableRendererForTesting(testColumnAmount, testRowAmount)
 	for i, border := range renderer.columnBorders {
 		assert.Equal(t, float32(2), border.StrokeWidth, "Border with number "+strconv.Itoa(i)+" does not have correct stroke width")
 		assert.Equal(t, color.Black, border.StrokeColor, "Border with number "+strconv.Itoa(i)+" does not have correct stroke color")
